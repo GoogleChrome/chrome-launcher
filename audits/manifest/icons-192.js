5932ef54a70e765699479e627b8d51e0eaaac2c1
Brendan Kenny <bckenny@google.com>
2016-03-24 13:41:22 -0700

    add a few more icon-192 tests
    also fixes a TypeError in the icon-192 audit when a manifest has an icon with no list of sizes

--- audits/manifest/icons-192.js
+++ audits/manifest/icons-192.js
@@ -32,7 +32,8 @@ class ManifestIcons192 {
 
     if (manifest && manifest.icons && manifest.icons.value) {
       const icons192 = manifest.icons.value.find(icon => {
-        return icon.value.sizes.value.indexOf('192x192') !== -1;
+        const sizesArray = icon.value.sizes.value;
+        return sizesArray && sizesArray.indexOf('192x192') !== -1;
       });
       hasIcons = (!!icons192);
     }
