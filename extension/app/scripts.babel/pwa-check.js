6aab09afae8f303fad1c574754cd9824e2b5f9bf
Sam Saccone <samccone@gmail.com>
2016-04-07 20:35:10 -0700

    extension: Move unrelated extension logic.
    All formatting logic has been moved to background.js so that pwa-check
    can remain pure.

--- extension/app/scripts.babel/pwa-check.js
+++ extension/app/scripts.babel/pwa-check.js
@@ -57,55 +57,9 @@ const aggregators = [
   require('../../../aggregators/is-sized-for-mobile-screen')
 ];
 
-function escapeHTML(str) {
-  return str.replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;')
-    .replace(/>/g, '&gt;')
-    .replace(/</g, '&lt;')
-    .replace(/`/g, '&#96;');
-}
-
-function createResultsHTML(results) {
-  let resultsHTML = '';
-
-  results.forEach(item => {
-    const score = (item.score.overall * 100).toFixed(0);
-    const groupHasErrors = (score < 100);
-    const groupClass = 'group ' +
-        (groupHasErrors ? 'errors expanded' : 'no-errors collapsed');
-
-    let groupHTML = '';
-    item.score.subItems.forEach(subitem => {
-      const debugString = subitem.debugString ? ` title="${escapeHTML(subitem.debugString)}"` : '';
-
-      // TODO: make this work with numeric values.
-      const status = subitem.value ?
-          `<span class="pass" ${debugString}>Pass</span>` :
-          `<span class="fail" ${debugString}>Fail</span>`;
-      groupHTML += `<li>${escapeHTML(subitem.description)}: ${status}</li>`;
-    });
-
-    resultsHTML +=
-      `<li class="${groupClass}">
-        <span class="group-name">${escapeHTML(item.name)}</span>
-        <span class="group-score">(${score}%)</span>
-        <ul>
-          ${groupHTML}
-        </ul>
-      </li>`;
-  });
-
-  return resultsHTML;
-}
-
 export function runPwaAudits() {
   return GatherScheduler
     .run(gatherers, {driver, url: driver.getCurrentTabURL()})
     .then(artifacts => Auditor.audit(artifacts, audits))
-    .then(results => Aggregator.aggregate(aggregators, results))
-    .then(results => {
-      return createResultsHTML(results);
-    })
-    .catch(err => `<div class="error">Unable to audit page: ${escapeHTML(err.message)}</div>`);
+    .then(results => Aggregator.aggregate(aggregators, results));
 }
