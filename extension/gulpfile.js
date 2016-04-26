bdf31f11a73480c186c3af98f7b0e9e92706bb40
Brendan Kenny <bckenny@google.com>
2016-04-25 17:44:24 -0700

    remove more extension dependencies

--- extension/gulpfile.js
+++ extension/gulpfile.js
@@ -9,12 +9,8 @@ const browserify = require('browserify');
 const chromeManifest = require('gulp-chrome-manifest');
 const debug = require('gulp-debug');
 const eslint = require('gulp-eslint');
-const gulpIf = require('gulp-if');
 const livereload = require('gulp-livereload');
-const cleanCss = require('gulp-clean-css');
-const minifyHtml = require('gulp-minify-html');
 const tap = require('gulp-tap');
-const useref = require('gulp-useref');
 const zip = require('gulp-zip');
 
 gulp.task('extras', () => {
@@ -47,11 +43,13 @@ gulp.task('images', () => {
   .pipe(gulp.dest('dist/images'));
 });
 
+gulp.task('css', () => {
+  return gulp.src('app/styles/**/*.css')
+  .pipe(gulp.dest('dist/styles'));
+});
+
 gulp.task('html', () => {
   return gulp.src('app/*.html')
-  .pipe(useref({searchPath: ['.tmp', 'app', '.']}))
-  .pipe(gulpIf('*.css', cleanCss({compatibility: '*'})))
-  .pipe(gulpIf('*.html', minifyHtml({conditionals: true, loose: true})))
   .pipe(gulp.dest('dist'));
 });
 
@@ -67,7 +65,6 @@ gulp.task('chromeManifest', () => {
   };
   return gulp.src('app/manifest.json')
   .pipe(chromeManifest(manifestOpts))
-  .pipe(gulpIf('*.css', cleanCss({compatibility: '*'})))
   .pipe(gulp.dest('dist'));
 });
 
@@ -128,7 +125,7 @@ gulp.task('package', function() {
 gulp.task('build', cb => {
   runSequence(
     'lint', 'browserify', 'chromeManifest',
-    ['html', 'images', 'extras'], cb);
+    ['html', 'images', 'css', 'extras'], cb);
 });
 
 gulp.task('default', ['clean'], cb => {
