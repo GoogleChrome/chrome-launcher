ec140b93670c96fcd27563fa5dbc7f1e77816724
Paul Irish <paul.irish@gmail.com>
2016-06-01 10:47:06 -0700

    fix strict mode in test/audits/audit.js

--- test/src/audits/audit.js
+++ test/src/audits/audit.js
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+'use strict';
+
 const Audit = require('../../../src/audits/audit.js');
 const assert = require('assert');
-
-'use strict';
 
 /* eslint-env mocha */
 
