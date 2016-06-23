0c467d3f2a3f296030305e48e03b608029173d36
Paul Irish <paul.irish@gmail.com>
2016-06-23 21:06:16 +0100

    Fix devtools-timeline-model tests
    Investigation underway

--- lighthouse-core/test/lib/traces/devtools-timeline-model.js
+++ lighthouse-core/test/lib/traces/devtools-timeline-model.js
@@ -30,21 +30,21 @@ describe('DevTools Timeline Model', function() {
   });
 
   it('metrics returned are expected', () => {
-    assert.equal(model.timelineModel().mainThreadEvents().length, 7228);
+    assert.equal(model.timelineModel().mainThreadEvents().length, 7756);
     assert.equal(model.interactionModel().interactionRecords().length, 0);
     assert.equal(model.frameModel().frames().length, 16);
   });
 
   it('top-down profile', () => {
     const leavesCount = model.topDown().children.size;
-    assert.equal(leavesCount, 28);
+    assert.equal(leavesCount, 27);
     const time = model.topDown().totalTime.toFixed(2);
     assert.equal(time, '559.21');
   });
 
   it('bottom-up profile', () => {
     const leavesCount = model.bottomUp().children.size;
-    assert.equal(leavesCount, 243);
+    assert.equal(leavesCount, 242);
     const topCosts = [...model.bottomUpGroupBy('URL').children.values()];
     const time = topCosts[1].totalTime.toFixed(2);
     const url = topCosts[1].id;
@@ -55,7 +55,7 @@ describe('DevTools Timeline Model', function() {
   it('bottom-up profile - group by eventname', () => {
     const bottomUpByName = model.bottomUpGroupBy('EventName');
     const leavesCount = bottomUpByName.children.size;
-    assert.equal(leavesCount, 15);
+    assert.equal(leavesCount, 14);
     const topCosts = [...bottomUpByName.children.values()];
     const time = topCosts[0].selfTime.toFixed(2);
     const name = topCosts[0].id;
