3e632f976b659f50de161df40fa5d69e7e913fb2
Paul Irish <paul.irish@gmail.com>
2016-07-27 12:19:20 -0400

    smoketest: do full default run against our basic html page. (#534)

--- lighthouse-cli/scripts/run-smoke-tests.sh
+++ lighthouse-cli/scripts/run-smoke-tests.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-cd lighthouse-cli/test/fixtures && python -m SimpleHTTPServer 9999 &
+cd lighthouse-cli/test/fixtures && python -m SimpleHTTPServer 10200 &
 
 NODE=$([ $(node -v | grep -E "v4") ] && echo "node --harmony" || echo "node")
-config="$PWD/lighthouse-cli/test/fixtures/smoketest-config.json"
-flags="--config-path=$config"
+#config="$PWD/lighthouse-cli/test/fixtures/smoketest-config.json"
+#flags="--config-path=$config"
 
 offline200result="URL responds with a 200 when offline"
 
-$NODE lighthouse-cli $flags http://localhost:9999/online-only.html > results
+# run default lighthouse run against a boring basic page
+$NODE lighthouse-cli --quiet http://localhost:10200/online-only.html > results
 
 # test that we have results
 if ! grep -q "$offline200result" results; then
@@ -34,10 +35,10 @@ fi
 #
 # # test mojibrush which should pass the offline test
 # $NODE lighthouse-cli $flags https://www.moji-brush.com > results
-# 
+#
 # if ! grep -q "$offline200result: true" results; then
 #   echo "Fail! offline ready site did not work while offline"
 #   cat results
 #   exit 1
 # fi
-# 
+#
