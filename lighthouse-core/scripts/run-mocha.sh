59fdea9dce728ec755e7c516cdbff2d1eacf3ddf
Paul Irish <paul.irish@gmail.com>
2016-08-01 16:10:54 -0700

    Refactor npm scripts for mocha

---
+++ lighthouse-core/scripts/run-mocha.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+flag=$1
+
+function _runmocha() {
+  mocha $2 $__node_harmony $(find $1/test -name '*.js') --timeout 60000;
+}
+
+if [ "$flag" == '--watch' ]; then
+    _runmocha '*' '--watch'
+elif [ "$flag" == '--cli' ]; then
+    _runmocha 'lighthouse-cli'
+else
+    _runmocha 'lighthouse-cli' && _runmocha 'lighthouse-core'
+fi
