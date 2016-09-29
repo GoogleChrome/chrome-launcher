fb2535b8eb037c81c19a15ef9644aa5f20a3e99b
Paul Irish <paul.irish@gmail.com>
2016-09-29 15:57:27 -0700

    DBW: Add audit for usage of document.write() (#716)
    * Add DWB audit for document.write

---
+++ lighthouse-core/audits/dobetterweb/no-document-write.js
@@ -0,0 +1,71 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Audit a page to see if it's using document.write()
+ */
+
+'use strict';
+
+const Audit = require('../audit');
+const Formatter = require('../../formatters/formatter');
+
+class NoDocWriteAudit extends Audit {
+
+  /**
+   * @return {!AuditMeta}
+   */
+  static get meta() {
+    return {
+      category: 'Performance',
+      name: 'no-document-write',
+      description: 'Site does not use document.write()',
+      helpText: 'Consider using <code>&lt;script async></code> to load scripts. <code>document.write()</code> is considered <a href="https://developers.google.com/web/updates/2016/08/removing-document-write" target="_blank">harmful for performance</a>.',
+      requiredArtifacts: ['URL', 'DocWriteUse']
+    };
+  }
+
+  /**
+   * @param {!Artifacts} artifacts
+   * @return {!AuditResult}
+   */
+  static audit(artifacts) {
+    if (typeof artifacts.DocWriteUse === 'undefined' ||
+        artifacts.DocWriteUse === -1) {
+      return NoDocWriteAudit.generateAuditResult({
+        rawValue: -1,
+        debugString: 'DocWriteUse gatherer did not run'
+      });
+    }
+
+    const results = artifacts.DocWriteUse.usage.map(err => {
+      return Object.assign({
+        misc: `(line: ${err.line}, col: ${err.col})`
+      }, err);
+    });
+
+    return NoDocWriteAudit.generateAuditResult({
+      rawValue: results.length === 0,
+      extendedInfo: {
+        formatter: Formatter.SUPPORTED_FORMATS.URLLIST,
+        value: results
+      }
+    });
+  }
+}
+
+module.exports = NoDocWriteAudit;
---
+++ lighthouse-core/test/audits/dobetterweb/no-document-write-test.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const DocWriteUseAudit = require('../../../audits/dobetterweb/no-document-write.js');
+const assert = require('assert');
+
+const URL = 'https://example.com';
+
+/* eslint-env mocha */
+
+describe('Page does not use document.write()', () => {
+  it('fails when no input present', () => {
+    const auditResult = DocWriteUseAudit.audit({});
+    assert.equal(auditResult.rawValue, -1);
+    assert.ok(auditResult.debugString);
+  });
+
+  it('passes when document.write() is not used', () => {
+    const auditResult = DocWriteUseAudit.audit({
+      DocWriteUse: {usage: []},
+      URL: {finalUrl: URL},
+    });
+    assert.equal(auditResult.rawValue, true);
+    assert.equal(auditResult.extendedInfo.value.length, 0);
+  });
+
+  it('fails when document.write() is used', () => {
+    const auditResult = DocWriteUseAudit.audit({
+      DocWriteUse: {
+        usage: [
+          {url: 'http://example.com/one', line: 1, col: 1},
+          {url: 'http://example.com/two', line: 10, col: 1},
+          {url: 'http://example2.com/two', line: 2, col: 22}
+        ]
+      },
+      URL: {finalUrl: URL},
+    });
+    assert.equal(auditResult.rawValue, false);
+    assert.equal(auditResult.extendedInfo.value.length, 3);
+  });
+});
