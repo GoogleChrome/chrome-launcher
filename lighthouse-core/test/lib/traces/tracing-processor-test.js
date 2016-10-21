285260abd03e8298f136dad9068bed44c027847c
Paul Irish <paul.irish@gmail.com>
2016-10-21 14:07:29 -0700

    test: add tests for TracingProcessor.getLogNormalDistribution

--- lighthouse-core/test/lib/traces/tracing-processor-test.js
+++ lighthouse-core/test/lib/traces/tracing-processor-test.js
@@ -158,5 +158,29 @@ describe('TracingProcessor lib', () => {
       const expected = createRiskPercentiles([1], [16 + duration2]);
       assert.deepEqual(results, expected);
     });
+
+    it('creates a log normal distribution', () => {
+      // This curve plotted with the below percentile assertions
+      // https://www.desmos.com/calculator/vjk2rwd17y
+
+      const median = 5000;
+      const pODM = 3500;
+      const distribution = TracingProcessor.getLogNormalDistribution(median, pODM);
+
+      function getPct(distribution, value) {
+        return distribution.computeComplementaryPercentile(value).toFixed(2);
+      }
+      assert.equal(typeof distribution.computeComplementaryPercentile, 'function');
+      assert.equal(getPct(distribution, 2000), '1.00', 'pct for 2000 does not match');
+      assert.equal(getPct(distribution, 3000), '0.98', 'pct for 3000 does not match');
+      assert.equal(getPct(distribution, 3500), '0.92', 'pct for 3500 does not match');
+      assert.equal(getPct(distribution, 4000), '0.81', 'pct for 4000 does not match');
+      assert.equal(getPct(distribution, 5000), '0.50', 'pct for 5000 does not match');
+      assert.equal(getPct(distribution, 6000), '0.24', 'pct for 6000 does not match');
+      assert.equal(getPct(distribution, 7000), '0.09', 'pct for 7000 does not match');
+      assert.equal(getPct(distribution, 8000), '0.03', 'pct for 8000 does not match');
+      assert.equal(getPct(distribution, 9000), '0.01', 'pct for 9000 does not match');
+      assert.equal(getPct(distribution, 10000), '0.00', 'pct for 10000 does not match');
+    });
   });
 });
