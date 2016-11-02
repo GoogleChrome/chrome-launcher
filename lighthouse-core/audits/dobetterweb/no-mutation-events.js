056c2b108e53dfa7bb9702a1081311b4943bea5f
Eric Bidelman <ebidel@gmail.com>
2016-11-02 01:36:23 -0700

    DBW: add no Mutation Events audit (#786)

---
+++ lighthouse-core/audits/dobetterweb/no-mutation-events.js
@@ -0,0 +1,99 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Audit a page to see if it is using Mutation Events (and suggest
+ *     MutationObservers instead).
+ */
+
+'use strict';
+
+const url = require('url');
+const Audit = require('../audit');
+const Formatter = require('../../formatters/formatter');
+
+class NoMutationEventsAudit extends Audit {
+
+  static get MUTATION_EVENTS() {
+    return [
+      'DOMAttrModified',
+      'DOMAttributeNameChanged',
+      'DOMCharacterDataModified',
+      'DOMElementNameChanged',
+      'DOMNodeInserted',
+      'DOMNodeInsertedIntoDocument',
+      'DOMNodeRemoved',
+      'DOMNodeRemovedFromDocument',
+      'DOMSubtreeModified'
+    ];
+  }
+
+  /**
+   * @return {!AuditMeta}
+   */
+  static get meta() {
+    return {
+      category: 'JavaScript',
+      name: 'no-mutation-events',
+      description: 'Site does not use Mutation Events (at the page level) in its own scripts',
+      helpText: 'Using <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mutation_events" target="_blank">Mutation events</a> degrades application performance. They are deprecated in the DOM events spec, replaced by <a href="https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver" target="_blank">MutationObservers</a>.',
+      requiredArtifacts: ['URL', 'PageLevelEventListeners']
+    };
+  }
+
+  /**
+   * @param {!Artifacts} artifacts
+   * @return {!AuditResult}
+   */
+  static audit(artifacts) {
+    if (typeof artifacts.PageLevelEventListeners === 'undefined' ||
+        artifacts.PageLevelEventListeners === -1) {
+      return NoMutationEventsAudit.generateAuditResult({
+        rawValue: -1,
+        debugString: 'PageLevelEventListeners gatherer did not run'
+      });
+    } else if (artifacts.PageLevelEventListeners.rawValue === -1) {
+      return NoMutationEventsAudit.generateAuditResult(artifacts.PageLevelEventListeners);
+    }
+
+    const listeners = artifacts.PageLevelEventListeners;
+
+    const pageHost = url.parse(artifacts.URL.finalUrl).host;
+
+    const results = listeners.filter(loc => {
+      const isMutationEvent = this.MUTATION_EVENTS.indexOf(loc.type) !== -1;
+      const sameHost = url.parse(loc.url).host === pageHost;
+      return sameHost && isMutationEvent;
+    }).map(loc => {
+      const handler = loc.handler ? loc.handler.description : '...';
+      return Object.assign({
+        label: `line: ${loc.line}, col: ${loc.col}`,
+        code: `${loc.objectId}.addEventListener('${loc.type}', ${handler})`
+      }, loc);
+    });
+
+    return NoMutationEventsAudit.generateAuditResult({
+      rawValue: results.length === 0,
+      extendedInfo: {
+        formatter: Formatter.SUPPORTED_FORMATS.URLLIST,
+        value: results
+      }
+    });
+  }
+}
+
+module.exports = NoMutationEventsAudit;
---
+++ lighthouse-core/test/audits/dobetterweb/no-mutation-events-test.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const NoMutationEventsAudit = require('../../../audits/dobetterweb/no-mutation-events.js');
+const assert = require('assert');
+
+const fixtureData = require('../../fixtures/page-level-event-listeners.json');
+
+const URL = 'https://example.com';
+
+/* eslint-env mocha */
+
+describe('Page does not use mutation events', () => {
+  it('it returns error value when no input present', () => {
+    const auditResult = NoMutationEventsAudit.audit({});
+    assert.equal(auditResult.rawValue, -1);
+    assert.ok(auditResult.debugString);
+  });
+
+  it('passes when mutation events are not used', () => {
+    const auditResult = NoMutationEventsAudit.audit({
+      PageLevelEventListeners: [],
+      URL: {finalUrl: URL},
+    });
+    assert.equal(auditResult.rawValue, true);
+    assert.equal(auditResult.extendedInfo.value.length, 0);
+  });
+
+  it('fails when mutation events are used on the origin', () => {
+    const auditResult = NoMutationEventsAudit.audit({
+      PageLevelEventListeners: fixtureData,
+      URL: {finalUrl: URL},
+    });
+    assert.equal(auditResult.rawValue, false);
+    assert.equal(auditResult.extendedInfo.value.length, 3);
+  });
+});
