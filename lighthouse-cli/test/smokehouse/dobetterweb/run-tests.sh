ffe0afda4ac2c3e38b92d43bc28378fee7bef8f5
Eric Bidelman <ebidel@gmail.com>
2016-11-01 21:09:00 -0700

    Add DBW to smoke tests (#843)

---
+++ lighthouse-cli/test/smokehouse/dobetterweb/run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+node lighthouse-cli/test/fixtures/static-server.js &
+
+sleep 0.5s
+
+config="lighthouse-core/config/dobetterweb.json"
+expectations="lighthouse-cli/test/smokehouse/dobetterweb/dbw-expectations.js"
+
+# run smoketest on DBW test page.
+npm run -s smokehouse -- --config-path=$config --expectations-path=$expectations
+exit_code=$?
+
+# kill test servers
+kill $(jobs -p)
+
+exit "$exit_code"
