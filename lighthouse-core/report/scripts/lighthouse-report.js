fbffab839852e19162319faab354d1733bdc4634
Paul Irish <paul.irish@gmail.com>
2016-12-02 16:38:24 -0800

    move helpText toggle to pure CSS

--- lighthouse-core/report/scripts/lighthouse-report.js
+++ lighthouse-core/report/scripts/lighthouse-report.js
@@ -19,40 +19,9 @@
 
 'use strict';
 
-/* Using ES5 to broaden support */
-function LighthouseReport() {
-  this.printButton = document.querySelector('.js-print');
-  this.viewUserFeature = document.querySelector('.js-report-by-user-feature');
-  this.viewTechnology = document.querySelector('.js-report-by-technology');
-  this.reportItems = document.querySelectorAll('.report-section__item');
-  this.helpToggles = document.querySelectorAll('.subitem__help-toggle');
-
-  this.toggleHelpText = this.toggleHelpText.bind(this);
-
-  this.addEventListeners();
-}
-
-LighthouseReport.prototype = {
-
-  onPrint: function() {
-    window.print();
-  },
-
-  toggleHelpText: function(e) {
-    const item = e.currentTarget.closest('.subitem');
-    item.classList.toggle('--show-help');
-  },
-
-  addEventListeners: function() {
-    this.printButton.addEventListener('click', this.onPrint);
-    [...this.helpToggles].forEach(node => {
-      node.addEventListener('click', this.toggleHelpText);
-    });
-  }
-};
-
 window.addEventListener('DOMContentLoaded', _ => {
-  // TODO: remove these two comments when we upgrade to latest eslint
-  // eslint-disable-next-line no-new
-  new LighthouseReport();
+  const printButton = document.querySelector('.js-print');
+  printButton.addEventListener('click', _ => {
+    window.print();
+  });
 });
