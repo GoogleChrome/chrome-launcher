8352aa89712c7dd8b33390c988578391cc822f25
Brendan Kenny <bckenny@gmail.com>
2016-12-20 19:42:16 -0800

    Remove node URL parsing while bugs are being fixed (#1187)

--- lighthouse-core/test/lib/url-shim-test.js
+++ lighthouse-core/test/lib/url-shim-test.js
@@ -1,5 +1,4 @@
 /**
- * @license
  * Copyright 2016 Google Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,15 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/**
- * URL shim so we keep our code DRY
- */
-
 'use strict';
 
-/* global self */
+/* eslint-env mocha */
 
-// TODO: Add back node require('url').URL parsing when bug is resolved:
-// https://github.com/GoogleChrome/lighthouse/issues/1186
-module.exports = (typeof self !== 'undefined' && self.URL) || require('whatwg-url').URL;
+const URL = require('../../lib/url-shim');
+const assert = require('assert');
+
+describe('URL Shim', () => {
+  it('handles URLs beginning with multiple digits', () => {
+    // from https://github.com/GoogleChrome/lighthouse/issues/1186
+    const url = 'http://5321212.fls.doubleclick.net/activityi;src=5321212;type=unvsn_un;cat=unvsn_uv;ord=7762287885264.98?';
+    assert.doesNotThrow(_ => new URL(url));
+  });
+});
