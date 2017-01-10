7811775ca93569cdaffe89c4f9e96bb576a810c4
Paul Irish <paul.irish@gmail.com>
2017-01-09 20:26:02 -0800

    Add more chrome flags for testing. (#1416)

--- lighthouse-cli/chrome-launcher.ts
+++ lighthouse-cli/chrome-launcher.ts
@@ -63,10 +63,24 @@ class ChromeLauncher {
   flags() {
     const flags = [
       `--remote-debugging-port=${this.port}`,
-      '--disable-extensions',
+      // Disable built-in Google Translate service
       '--disable-translate',
+      // Disable all chrome extensions entirely
+      '--disable-extensions',
+      // Disable various background network services, including extension updating,
+      //   safe browsing service, upgrade detector, translate, UMA
+      '--disable-background-networking',
+      // Disable fetching safebrowsing lists, likely redundant due to disable-background-networking
+      '--safebrowsing-disable-auto-update',
+      // Disable syncing to a Google account
+      '--disable-sync',
+      // Disable reporting to UMA, but allows for collection
+      '--metrics-recording-only',
+      // Disable installation of default apps on first run
       '--disable-default-apps',
+      // Skip first run wizards
       '--no-first-run',
+      // Place Chrome profile in a custom location we'll rm -rf later
       `--user-data-dir=${this.TMP_PROFILE_DIR}`
     ];
 
