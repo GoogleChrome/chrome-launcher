49fdcbb5dbdcc5e18f1e3cddc28d8d98ba448ca4
XhmikosR <xhmikosr@gmail.com>
2017-01-29 12:46:42 +0200

    Use download-chrome.sh to download Chrome on Windows too.
    wget that ships with MSYS needs `--no-check-certificate` otherwise it fails for SNI.
    
    Also, specified `-q` so that the output isn't bloated.

--- lighthouse-core/scripts/download-chrome.sh
+++ lighthouse-core/scripts/download-chrome.sh
@@ -2,15 +2,19 @@
 
 # Download chrome inside of our CI env.
 
-if [ x"$LIGHTHOUSE_CHROMIUM_PATH" == x ]
-then
+if [ "$APPVEYOR" == "True" ]; then
+  url="https://download-chromium.appspot.com/dl/Win?type=snapshots"
+else
+  url="https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots"
+fi
+
+if [ x"$LIGHTHOUSE_CHROMIUM_PATH" == x ]; then
   echo "Error: Environment variable LIGHTHOUSE_CHROMIUM_PATH not set"
   exit 1
 fi
 
-if [ -e "$LIGHTHOUSE_CHROMIUM_PATH" ]
-then
+if [ -e "$LIGHTHOUSE_CHROMIUM_PATH" ]; then
   echo "cached chrome found"
 else
-  wget 'https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots' -O chrome.zip && unzip chrome.zip
+  wget "$url" --no-check-certificate -q -O chrome.zip && unzip chrome.zip
 fi
