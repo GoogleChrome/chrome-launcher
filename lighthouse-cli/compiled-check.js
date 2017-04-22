b25f535eb1aa74f3fd9b1f3c6096879b32c8f0e1
Paul Irish <paul.irish@gmail.com>
2017-04-21 17:23:43 -0700

    CLI not compiled note should use yarn. (#2047)
    [skip ci]

--- lighthouse-cli/compiled-check.js
+++ lighthouse-cli/compiled-check.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = function(filename) {
   if (!fs.existsSync(path.join(__dirname, filename))) {
     console.log('Oops! Looks like the CLI needs to be compiled. Please run:');
-    console.log('   npm run install-cli');
+    console.log('   yarn install-cli; yarn build-cli');
     console.log('More at: https://github.com/GoogleChrome/lighthouse#develop');
     process.exit(1);
   }
