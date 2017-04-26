05a068af5a8dc9d985b921980d7ec7e16164a9c2
Matt Busche <mrbusche@gmail.com>
2017-04-25 23:46:05 -0500

    Update outdated link for "Web App Install Banners" article #2035 (#2068)

--- lighthouse-core/audits/webapp-install-banner.js
+++ lighthouse-core/audits/webapp-install-banner.js
@@ -39,7 +39,7 @@ class WebappInstallBanner extends Audit {
       category: 'PWA',
       name: 'webapp-install-banner',
       description: 'User can be prompted to Install the Web App',
-      helpText: 'While users can manually add your site to their homescreen, the [prompt (aka app install banner)](https://developers.google.com/web/updates/2015/03/increasing-engagement-with-app-install-banners-in-chrome-for-android) will proactively prompt the user to install the app if the various requirements are met and the user has moderate engagement with your site.',
+      helpText: 'While users can manually add your site to their homescreen, the [prompt (aka app install banner)](https://developers.google.com/web/fundamentals/engage-and-retain/app-install-banners/) will proactively prompt the user to install the app if the various requirements are met and the user has moderate engagement with your site.',
       requiredArtifacts: ['URL', 'ServiceWorker', 'Manifest']
     };
   }
