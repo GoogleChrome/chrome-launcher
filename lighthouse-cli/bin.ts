efaeb135fdfde07c737d37bb4ea61812e01e3fe2
Paul Irish <paul.irish@gmail.com>
2017-05-09 14:08:32 -0700

    Report-> domhtml renderer. Add Lighthouse Canary assets (#2184)

--- lighthouse-cli/bin.ts
+++ lighthouse-cli/bin.ts
@@ -132,7 +132,7 @@ Example: --output-path=./lighthouse-results.html`,
         // default values
         .default('chrome-flags', '')
         .default('disable-cpu-throttling', false)
-        .default('output', Printer.GetValidOutputOptions()[Printer.OutputMode.html])
+        .default('output', Printer.GetValidOutputOptions()[Printer.OutputMode.domhtml])
         .default('port', 9222)
         .default('max-wait-for-load', Driver.MAX_WAIT_FOR_FULLY_LOADED)
         .check((argv: {listAllAudits?: boolean, listTraceCategories?: boolean, _: Array<any>}) => {
--- lighthouse-extension/app/src/lighthouse-background.js
+++ lighthouse-extension/app/src/lighthouse-background.js
@@ -23,7 +23,7 @@ const Config = require('../../../lighthouse-core/config/config');
 const defaultConfig = require('../../../lighthouse-core/config/default.js');
 const log = require('../../../lighthouse-core/lib/log');
 
-const ReportGenerator = require('../../../lighthouse-core/report/report-generator');
+const ReportGeneratorV2 = require('../../../lighthouse-core/report/v2/report-generator');
 
 const STORAGE_KEY = 'lighthouse_audits';
 const SETTINGS_KEY = 'lighthouse_settings';
@@ -168,15 +168,10 @@ window.runLighthouseInWorker = function(port, url, options, categoryIDs) {
  * @param {!string} reportContext Where the report is going
  * @return {!string} Blob URL of the report (or error page) HTML
  */
-window.createReportPageAsBlob = function(results, reportContext) {
+window.createReportPageAsBlob = function(results) {
   performance.mark('report-start');
-  const reportGenerator = new ReportGenerator();
-  let html;
-  try {
-    html = reportGenerator.generateHTML(results, reportContext);
-  } catch (err) {
-    html = reportGenerator.renderException(err, results);
-  }
+  const html = new ReportGeneratorV2().generateReportHtml(results);
+
   const blob = new Blob([html], {type: 'text/html'});
   const blobURL = window.URL.createObjectURL(blob);
 
