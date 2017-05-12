2ea5fed493388f7bc44e3e179f3a0d889808a0b7
Kayce Basques <kayce@basqu.es>
2017-05-12 12:41:05 -0700

    update helpText for "button name" audit (#2229)
    * update helpText for "button name" audit
    
    * update "button name" helpText

--- lighthouse-core/audits/accessibility/button-name.js
+++ lighthouse-core/audits/accessibility/button-name.js
@@ -33,8 +33,9 @@ class ButtonName extends AxeAudit {
       category: 'Accessibility',
       name: 'button-name',
       description: 'Buttons have an accessible name.',
-      helpText: 'An accessible name helps convey the purpose of a button. Without one, a screen ' +
-          'reader will only announce the word "button".',
+      helpText: 'When a button doesn\'t have an accessible name, screen readers announce it as ' +
+          '"button", making it unusable for users who rely on screen readers. ' +
+          '[Learn more](https://developers.google.com/web/tools/lighthouse/audits/button-name).',
       requiredArtifacts: ['Accessibility']
     };
   }
