0e4652efecbd6cef45c514185109cd723c2a0c66
Ward Peeters <ward@coding-tech.com>
2017-07-20 01:19:13 +0200

    Fix chrome finder on linux/osx when process.env isn't populated (https://github.com/GoogleChrome/lighthouse/pull/2687)

--- chrome-finder.ts
+++ chrome-finder.ts
@@ -53,11 +53,17 @@ export function darwin() {
     {regex: /^\/Applications\/.*Chrome Canary.app/, weight: 101},
     {regex: /^\/Volumes\/.*Chrome.app/, weight: -2},
     {regex: /^\/Volumes\/.*Chrome Canary.app/, weight: -1},
-    {regex: new RegExp(process.env.LIGHTHOUSE_CHROMIUM_PATH), weight: 150},
-    {regex: new RegExp(process.env.CHROME_PATH), weight: 151}
   ];
-  // clang-format on
 
+  if (process.env.LIGHTHOUSE_CHROMIUM_PATH) {
+    priorities.push({regex: new RegExp(process.env.LIGHTHOUSE_CHROMIUM_PATH), weight: 150});
+  }
+
+  if (process.env.CHROME_PATH) {
+    priorities.push({regex: new RegExp(process.env.CHROME_PATH), weight: 151});
+  }
+
+  // clang-format on
   return sort(installations, priorities);
 }
 
@@ -124,11 +130,18 @@ export function linux() {
   }
 
   const priorities: Priorities = [
-    {regex: /chrome-wrapper$/, weight: 51}, {regex: /google-chrome-stable$/, weight: 50},
+    {regex: /chrome-wrapper$/, weight: 51},
+    {regex: /google-chrome-stable$/, weight: 50},
     {regex: /google-chrome$/, weight: 49},
-    {regex: new RegExp(process.env.LIGHTHOUSE_CHROMIUM_PATH), weight: 100},
-    {regex: new RegExp(process.env.CHROME_PATH), weight: 101}
   ];
+
+  if (process.env.LIGHTHOUSE_CHROMIUM_PATH) {
+    priorities.push({regex: new RegExp(process.env.LIGHTHOUSE_CHROMIUM_PATH), weight: 100});
+  }
+
+  if (process.env.CHROME_PATH) {
+    priorities.push({regex: new RegExp(process.env.CHROME_PATH), weight: 101});
+  }
 
   return sort(uniq(installations.filter(Boolean)), priorities);
 }
