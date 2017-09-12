cae8dad41d13bbae1c951eb988ecdb23a2d078f0
Paul Irish <paul.irish@gmail.com>
2017-09-12 10:58:35 -0700

    Add chrome flags used in chromedriver/puppeteer
    https://github.com/GoogleChrome/puppeteer/commit/4af0911b90773e59c3ff552f6c4cc6e42af715a0
    https://cs.chromium.org/chromium/src/chrome/test/chromedriver/chrome_launcher.cc?type=cs&q=f:chrome_launcher++kDesktopSwitches&sq=package:chromium&l=72

--- flags.ts
+++ flags.ts
@@ -6,15 +6,16 @@
 'use strict';
 
 export const DEFAULT_FLAGS = [
-  // Disable built-in Google Translate service
-  '--disable-translate',
   // Disable all chrome extensions entirely
   '--disable-extensions',
   // Disable various background network services, including extension updating,
   //   safe browsing service, upgrade detector, translate, UMA
   '--disable-background-networking',
-  // Disable fetching safebrowsing lists, likely redundant due to disable-background-networking
+  // Disable fetching safebrowsing lists. Otherwise requires a 500KB download immediately after launch.
+  // This flag is likely redundant due to disable-background-networking
   '--safebrowsing-disable-auto-update',
+  // Disable built-in Google Translate service
+  '--disable-translate',
   // Disable syncing to a Google account
   '--disable-sync',
   // Disable reporting to UMA, but allows for collection
@@ -25,4 +26,19 @@ export const DEFAULT_FLAGS = [
   '--mute-audio',
   // Skip first run wizards
   '--no-first-run',
+  
+  // Disable timers being throttled in background pages/tabs
+  '--disable-background-timer-throttling',
+  // Disables client-side phishing detection. Likely redundant due to the safebrowsing disable
+  '--disable-client-side-phishing-detection',
+  // Disable popup blocking
+  '--disable-popup-blocking',
+  // Reloading a page that came from a POST normally prompts the user.
+  '--disable-prompt-on-repost',
+  // Disable the a few things considered not appropriate for automation, e.g. infobar animations, password saving UI
+  '--enable-automation',
+  // Avoid potential instability of using Gnome Keyring or KDE wallet. crbug.com/571003
+  '--password-store=basic',
+  // Use mock keychain on mac to prevent blocking permissions dialogs
+  'use-mock-keychain',
 ];
