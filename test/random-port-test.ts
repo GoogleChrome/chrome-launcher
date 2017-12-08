5e425d3b9f2bb2706f9bd83467dd5ffe411a900f
Paul Irish <paul.irish@gmail.com>
2017-12-08 13:35:40 -0800

    fix tests.

--- test/random-port-test.ts
+++ test/random-port-test.ts
@@ -16,7 +16,7 @@
 'use strict';
 
 import * as assert from 'assert';
-import {getRandomPort} from '../random-port';
+import {getRandomPort} from '../src/random-port';
 
 describe('Random port generation', () => {
   it('generates a valid random port number', () => {
