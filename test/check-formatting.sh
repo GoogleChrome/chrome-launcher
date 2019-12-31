316a167e9e92ed59792ade14111bed0f05192c89
Cris Stringfellow <cris7fe@gmail.com>
2020-01-01 00:54:42 +0800

    Revert exposure of formatting test failure
    Tests now pass, so there is no need to print the intended format, because the intended format is now the format used in https://github.com/GoogleChrome/chrome-launcher/pull/184

--- test/check-formatting.sh
+++ test/check-formatting.sh
@@ -2,7 +2,6 @@
 
 check_formatting ()
 {
-  ./node_modules/.bin/clang-format -style=file $1
   diff -u <(cat $1) <(./node_modules/.bin/clang-format -style=file $1) &>/dev/null
   if [ $? -eq 1 ]
   then
